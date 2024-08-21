0aaf633c2dfb6186ed9fe548e7fe9bd072049649
Золотарева Ирина Александровна <Zolotareva.I@klgtu.local>
2024-08-21 16:40:01 +0200

    change paths

--- src/shared/api/base.ts
+++ src/shared/api/base.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const baseRequester = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://api.github.com/graphql",
+  // baseURL: import.meta.env.VITE_API_URL,
   headers: {
     Authorization: `Bearer ${import.meta.env.VITE_GITHUB_ACCESS_TOKEN}`,
     "Content-Type": "application/json",
