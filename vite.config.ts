c006befcc3b0d0cb564f03cfd2ba141fb376ca55
Золотарева Ирина Александровна <Zolotareva.I@klgtu.local>
2024-08-21 15:50:20 +0200

    ready for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/repository-search/",
   plugins: [react(), svgr(), graphqlLoader()],
   resolve: {
     alias: {
